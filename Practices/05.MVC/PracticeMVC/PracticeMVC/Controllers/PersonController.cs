f23143e0412901d6c21ab6aab3ce7585ecc91db4
DanlerLo <loaizad65@gmail.com>
2022-03-21 19:00:04 -0500

    Hola

---
+++ Practices/05.MVC/PracticeMVC/PracticeMVC/Controllers/PersonController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace PracticeMVC.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: PersonController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PersonController/Details/5
+        public ActionResult Details(int id)
+        {
+            Models.Person person = new Models.Person()
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                DateOfBirth = DateTime.Parse("1974/10/08"),
+                Sex = 'M'
+            };
+
+            return View(person);
+        }
+
+        // GET: PersonController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PersonController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PersonController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PersonController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Practices/05.MVC/PracticeMVC/PracticeMVC/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PracticeMVC.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public char Sex { get; set; }
+    }
+}
