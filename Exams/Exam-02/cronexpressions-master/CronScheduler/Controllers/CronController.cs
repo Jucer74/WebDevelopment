26da109bfeab413c39730c03c5c82b57a45633d8
Rosemberg-Diaz <diazmendezdaniel043@gmail.com>
2022-05-03 19:40:21 -0500

    CronExpression

---
+++ Exams/Exam-02/cronexpressions-master/CronScheduler/Controllers/CronController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CronScheduler.Controllers
+{
+    public class CronController : Controller
+    {
+        // GET: Crone
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Advance()
+        {
+            return View();
+        }
+    }
+}
---
+++ Exams/Exam-02/cronexpressions-master/CronScheduler/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CronScheduler.MvcApplication" Language="C#" %>
