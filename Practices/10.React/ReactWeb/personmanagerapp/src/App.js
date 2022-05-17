0c1849c5b0cd35985311628348ab267178ba5f21
Julio Robles <Jucer74@Hotmail.com>
2022-05-16 20:20:54 -0500

    Update React

--- Practices/10.React/ReactWeb/personmanagerapp/src/App.js
+++ Practices/10.React/ReactWeb/personmanagerapp/src/App.js
@@ -1,32 +1,23 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Layout } from './Layout';
-import { NotFound } from './components/NotFound';
-import { Home } from './components/Home';
-import { Login } from './components/Login';
-import { List } from './components/Persons';
-import { NavigationBar } from './components/NavigationBar';
-
 
 function App() {
   return (
     <div className="App">
-      <React.Fragment>
-        <NavigationBar />
-        <Layout>
-          <Router>
-            <Routes>
-              <Route exact path="/" element={<Home/>} />
-              <Route path="/Home" element={<Home/>} />
-              <Route path="/Persons" element={<List/>} />
-              <Route path="/Login" element={<Login/>} />
-              <Route element={<NotFound/>} />
-            </Routes>
-          </Router>
-        </Layout>
-      </React.Fragment>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
