140a6ef1f6ca826fa5c8d68ce0dc9d7f4c328b16
Julio Robles <Jucer74@Hotmail.com>
2022-05-16 22:09:15 -0500

    Update Menu

--- Practices/10.React/ReactWeb/personmanagerapp/src/App.js
+++ Practices/10.React/ReactWeb/personmanagerapp/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import { Layout } from './Layout';
+import { NoMatch } from './components/NoMatch';
+import { Home } from './components/Home';
+import { Login } from './components/Login';
+import { People } from './components/People';
+import { NavigationBar } from './components/NavigationBar';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.Fragment>
+        <NavigationBar />
+        <Layout>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="Home" element={<Home />} />
+              <Route path="People" element={<People />} />
+              <Route path="Login" element={<Login />} />
+              <Route element={<NoMatch/>} />
+            </Routes>
+          </Router>
+        </Layout>
+      </React.Fragment>
     </div>
   );
 }
