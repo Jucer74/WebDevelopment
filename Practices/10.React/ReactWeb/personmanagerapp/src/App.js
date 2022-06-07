6ca7cc5fc53ad0eaaa92f01f318f6369cc4a1d67
Juan-Camilorr <jcrrcamilo@gmail.com>
2022-06-07 11:48:45 -0500

    .

--- Practices/10.React/ReactWeb/personmanagerapp/src/App.js
+++ Practices/10.React/ReactWeb/personmanagerapp/src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Image, Navbar, Nav } from 'react-bootstrap';
-import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import { Layout } from './Layout';
 import { NoMatch } from './components/NoMatch';
 import { Home } from './components/Home';
 import { Login } from './components/Login';
-import { Personas } from './components/Personas';
+import { List, People } from './components/People';
 import { NavigationBar } from './components/NavigationBar';
 
 function App() {
@@ -20,10 +17,10 @@ function App() {
         <Layout>
           <Router>
             <Routes>
-              <Route exact path="/" element={<Home/>} />
-              <Route path="/Home" element={<Home/>} />
-              <Route path="/Personas" element={<Personas/>} />
-              <Route path="/Login" element={<Login/>} />
+              <Route path="/" element={<List />} />
+              <Route path="Home" element={<Home />} />
+              <Route path="People" element={<List />}  />
+              <Route path="Login" element={<Login />} />
               <Route element={<NoMatch/>} />
             </Routes>
           </Router>
--- Practices/10.React/ReactWeb/personmanagerapp/src/components/Personas.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Personas = () => (
-  <h1>Personas</h1>
-)
