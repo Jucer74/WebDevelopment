fd9fac7aa94b6df35b6b23ce1683d3c41e516d2a
Julio Robles <Jucer74@Hotmail.com>
2022-06-06 20:46:36 -0500

    Normalizar Proyecto

--- Practices/10.React/React/PeopleApp/People.Domain/Interfaces/Repositories/IPersonRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using People.Domain.Common;
-using People.Domain.Entities;
-
-namespace People.Domain.Interfaces.Repositories
-{
-   public interface IPersonRepository : IRepository<Person>
-   {
-   }
-}
