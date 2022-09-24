a4d87eab04eb4ad0969ca1ae6989d9a233dbf3ef
Julio Robles <Jucer74@Hotmail.com>
2022-09-24 12:14:15 -0500

    Initial Project

---
+++ Practices/05.WebAPI/EmployeesWeb/EmployeesWeb.Application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeesWeb.Application.Models
+{
+   public class ErrorViewModel
+   {
+      public string? RequestId { get; set; }
+
+      public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+   }
+}
