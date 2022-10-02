185ab45d31ce46998963577900dbe537ea3884d3
Julio Robles <Jucer74@Hotmail.com>
2022-10-01 23:04:37 -0500

    Create Employee.cs

---
+++ Practices/05.WebAPI/EmployeesWeb/EmployeesWeb.Application/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesWeb.Application.Models
+{
+   public class Employee
+   {
+      [Key]
+      public int Id { get; set; }
+
+      [Required(ErrorMessage = "The FirstName is required")]
+      public string? FirstName { get; set; }
+
+      [Required(ErrorMessage = "The LastBame is Required")]
+      public string? LastName { get; set; }
+
+      [Required(ErrorMessage = "The HireDate is Required")]
+      public DateTime HireDate{ get; set; }
+
+      [Required(ErrorMessage = "The Department is Required")]
+      public string Departmen { get; set; }
+   }
+}
