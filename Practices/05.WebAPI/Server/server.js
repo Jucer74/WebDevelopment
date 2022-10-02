f1ac72712e2cf20b90649cb7a5a6cd7eb2c79598
Julio Robles <Jucer74@Hotmail.com>
2022-10-01 22:13:55 -0500

    Clean ad Update

--- Practices/05.WebAPI/Server/server.js
+++ Practices/05.WebAPI/Server/server.js
@@ -4,16 +4,17 @@ const jsonServer = require('json-server')
 const jwt = require('jsonwebtoken')
 
 const server = jsonServer.create()
+
 const router = jsonServer.router('./EmployeesDB.json')
+
 const userdb = JSON.parse(fs.readFileSync('./users.json', 'UTF-8'))
+
+const SECRET_KEY = '123456789'
+const expiresIn = '1h'
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
 server.use(jsonServer.defaults());
-
-const SECRET_KEY = '123456789'
-
-const expiresIn = '1h'
 
 // Create a token from a payload 
 function createToken(payload){
@@ -29,52 +30,7 @@ function verifyToken(token){
 function isAuthenticated({email, password}){
   return userdb.users.findIndex(user => user.email === email && user.password === password) !== -1
 }
-
-// Register New User
-server.post('/auth/register', (req, res) => {
-  console.log("register endpoint called; request body:");
-  console.log(req.body);
-  const {email, password} = req.body;
-
-  if(isAuthenticated({email, password}) === true) {
-    const status = 401;
-    const message = 'Email and Password already exist';
-    res.status(status).json({status, message});
-    return
-  }
-
-fs.readFile("./users.json", (err, data) => {  
-    if (err) {
-      const status = 401
-      const message = err
-      res.status(status).json({status, message})
-      return
-    };
-
-    // Get current users data
-    var data = JSON.parse(data.toString());
-
-    // Get the id of last user
-    var last_item_id = data.users[data.users.length-1].id;
-
-    //Add new user
-    data.users.push({id: last_item_id + 1, email: email, password: password}); //add some data
-    var writeData = fs.writeFile("./users.json", JSON.stringify(data), (err, result) => {  // WRITE
-        if (err) {
-          const status = 401
-          const message = err
-          res.status(status).json({status, message})
-          return
-        }
-    });
-});
-
-// Create token for new user
-  const access_token = createToken({email, password})
-  console.log("Access Token:" + access_token);
-  res.status(200).json({access_token})
-})
-
+  
 // Login to one of the users from ./users.json
 server.post('/auth/login', (req, res) => {
   console.log("login endpoint called; request body:");
@@ -114,6 +70,51 @@ server.use(/^(?!\/auth).*$/,  (req, res, next) => {
     const message = 'Error access_token is revoked'
     res.status(status).json({status, message})
   }
+})
+
+// Register New User
+server.post('/auth/register', (req, res) => {
+  console.log("register endpoint called; request body:");
+  console.log(req.body);
+  const {email, password} = req.body;
+
+  if(isAuthenticated({email, password}) === true) {
+    const status = 401;
+    const message = 'Email and Password already exist';
+    res.status(status).json({status, message});
+    return
+  }
+
+  fs.readFile("./users.json", (err, data) => {  
+    if (err) {
+      const status = 401
+      const message = err
+      res.status(status).json({status, message})
+      return
+    };
+
+    // Get current users data
+    var data = JSON.parse(data.toString());
+
+    // Get the id of last user
+    var last_item_id = data.users[data.users.length-1].id;
+
+    //Add new user
+    data.users.push({id: last_item_id + 1, email: email, password: password}); //add some data
+    var writeData = fs.writeFile("./users.json", JSON.stringify(data), (err, result) => {  // WRITE
+        if (err) {
+          const status = 401
+          const message = err
+          res.status(status).json({status, message})
+          return
+        }
+    });
+  });
+
+  // Create token for new user
+  const access_token = createToken({email, password})
+  console.log("Access Token:" + access_token);
+  res.status(200).json({access_token})
 })
 
 server.use(router)
