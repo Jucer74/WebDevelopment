0267fbbbbb37c74de49768cb386e8ec738b2af27
apraezCristian <94486784+apraezCristian@users.noreply.github.com>
2023-04-23 01:36:39 -0500

    ..

---
+++ Exams/Exam-02/PricatMVC/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace PricatMVC.Models
+{
+    public class Category
+    {
+        [Required(ErrorMessage = "El Id es requerido")]
+        [DisplayName("Id")]
+        public int Id { get; set; } = 1;
+
+
+        [Required(ErrorMessage = "La descripción es requerida")]
+        [StringLength(50, ErrorMessage = "La Longitud maxima de la descripción es de 50 caracteres")]
+        [DisplayName("Descripción")]
+        public string Description { get; set; } = null!;
+    }
+}
---
+++ Exams/Exam-02/PricatMVC/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace PricatMVC.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage = "El Id es requerido")]
+        [DisplayName("Id")]
+        public int CategoryId { get; set; } = 0;
+
+        [Required(ErrorMessage = "La descripción es requerida")]
+        [StringLength(50, ErrorMessage = "La Longitud maxima de la descripción es de 50 caracteres")]
+        [DisplayName("Descripción")]
+        public string Description { get; set; } = null!;
+
+        [Required(ErrorMessage = "El Id es requerido")]
+        [DisplayName("Id")]
+        public int Id { get; set; } = 1;
+
+        [Required(ErrorMessage = "El EanCode es requerido")]
+        [DisplayName("EanCode")]
+        public int EanCode { get; set; } = 0;
+
+        [Required(ErrorMessage = "La unidad es requerida")]
+        [DisplayName("Unit")]
+        public string Unit { get; set; } = null!;
+    }
+}
