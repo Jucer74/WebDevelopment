50a732b13e148a283e238a3b8a879d7744278024
Juanjo271 <juanop556@hotmail.com>
2023-08-15 15:44:42 -0500

    avance 6

---
+++ Practices/02.Bootstrap/js/Scripts.js
@@ -0,0 +1,12 @@
+$('section.awSlider .carousel').carousel({
+	pause: "hover",
+  interval: 2000
+});
+
+var startImage = $('section.awSlider .item.active > img').attr('src');
+$('section.awSlider').append('<img src="' + startImage + '">');
+
+$('section.awSlider .carousel').on('slid.bs.carousel', function () {
+ var bscn = $(this).find('.item.active > img').attr('src');
+	$('section.awSlider > img').attr('src',bscn);
+});
