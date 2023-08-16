2ded421f60b1023f27f9b05b659c638b274fe703
Juanjo271 <juanop556@hotmail.com>
2023-08-15 21:12:48 -0500

    Clase 15/08/2023

---
+++ Practices/03.MVC/Ejercicio 1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ejercicio_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Practices/03.MVC/Ejercicio 1/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ejercicio_1.Models
+{
+    public class Student
+    {
+
+
+        [Key]
+        [Display (Name ="Id")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [Display(Name = "Nombre")]
+        public string FirstName { get; set; } = null!;
+        [Required(ErrorMessage = "El Apellido es requerido")]
+        public string LastName { get; set; } = null!;
+        [Required(ErrorMessage = "El Fecha de nacimiento es requerida")]
+        public DateTime DateOfBirth { get; set; }
+        [Required(ErrorMessage = "El sexo es requerido")]
+        public char Sex { get; set; } = 'M';  
+
+
+    }
+}
