f2b37034b729465dfa542a4b129ac26c660bfa0f
Miguel-Marin-Moreno <103012297+Miguel-Marin-Moreno@users.noreply.github.com>
2023-08-15 21:26:41 -0500

    StudentsMVC

---
+++ Practices/03.MVC/StudentsMVC/Controllers/StudentsController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentsMVC.Models;
+
+namespace StudentsMVC.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // Global Varaibles
+        private static List<Student> studentsList = LoadStudents(); 
+
+        // GET: StudentsController
+        public ActionResult Index()
+        {
+            return View(studentsList);
+        }
+
+        // GET: StudentsController/Details/5
+        public ActionResult Details(int id)
+
+        {   var student = studentsList.FirstOrDefault(x => x.Id == id);
+            return View();
+        }
+
+        // GET: StudentsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var student = studentsList.FirstOrDefault(x => x.Id == id);
+            return View();
+        }
+
+        // POST: StudentsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var student = studentsList.FirstOrDefault(x => x.Id == id);
+            return View();
+        }
+
+        // POST: StudentsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #region Private-Methods
+
+        private static List<Student> LoadStudents()
+        {
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student() { Id = 1, FirstName = "John", LastName = "Martinez", DateOfBirth = new DateTime(1998, 9, 10), Sex = 'M' });
+            students.Add(new Student() { Id = 1, FirstName = "Luis", LastName = "Diaz", DateOfBirth = new DateTime(1998, 8, 10), Sex = 'M' });
+            students.Add(new Student() { Id = 1, FirstName = "Juan", LastName = "Gonzales", DateOfBirth = new DateTime(1998, 7, 10), Sex = 'M' });
+            students.Add(new Student() { Id = 1, FirstName = "pablo", LastName = "Nazario", DateOfBirth = new DateTime(1998, 6, 10), Sex = 'M' });
+
+            return students;
+        }
+
+        #endregion Private-Methods
+
+    }
+}
---
+++ Practices/03.MVC/StudentsMVC/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsMVC.Models;
+
+public class Student
+{
+    [Key]
+    [Display(Name = "Id")]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "El Nombre es requqerido")]
+    [Display(Name = "Nombre")]
+    public string FirstName { get; set; } = null!;
+
+    [Required(ErrorMessage = "El Apellido es requqerido")]
+    public string LastName { get; set; } = null!;
+
+    [Required(ErrorMessage = "La fecha de Nacimiento es requqerid")]
+    public DateTime DateOfBirth { get; set; }
+
+    [Required(ErrorMessage = "Sexo requqerido")]
+    public char Sex { get; set; }= 'M';
+    
+
+
+
+}
