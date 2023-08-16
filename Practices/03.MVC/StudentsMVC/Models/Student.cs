ef9b64bae19d9eef893d3e7bc153843000b87bee
W-sebas23 <sebasmartinezr2014@gmail.com>
2023-08-15 20:24:30 -0500

    studentController

--- Practices/03.MVC/StudentsMVC/Models/Student.cs
+++ Practices/03.MVC/StudentsMVC/Models/Student.cs
@@ -1,6 +1,26 @@
-﻿namespace StudentsMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsMVC.Models;
+
+public class Student
 {
-    public class Student
-    {
-    }
+    [Key]
+    [Display(Name = "Id")]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "El nombre es requerido")]
+    [Display(Name = "Nombre")]
+    public string FirstName { get; set; } = null;
+
+    [Required(ErrorMessage = "El apellido es requerido")]
+    public string LastName { get; set; } = null;
+
+    [Required(ErrorMessage = "El cumpleaños es requerido")]
+    public DateTime DateOfBirth { get; set; }
+
+    [Required(ErrorMessage = "El sexo es requerido")]
+    public char Sex { get; set; } = 'M';
+    
+
+    
 }
