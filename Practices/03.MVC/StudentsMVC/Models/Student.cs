2b15384cdbf2cbd25817de74c02867fd79a59c5b
Santierazo <103299493+Santierazo@users.noreply.github.com>
2023-08-15 21:25:40 -0500

    MVC

---
+++ Practices/03.MVC/StudentsMVC/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsMVC.Models;
+
+public class Student
+{
+    [Key]
+    [Display(Name = "Id")]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "El Nombre es requerido")]
+    [Display(Name = "Nombre")]
+    public string FirstName { get; set; } = null!;
+    [Required(ErrorMessage = "El Apellido es requerido")]
+    [Display(Name = "Apellido")]
+    public string LastName { get; set; } = null!;
+    [Required(ErrorMessage = "El Fecha de nacimiento es requerida")]
+    [Display(Name = "Fecha de Nacimiento")]
+    public DateTime DateOfBirth { get; set; }
+    [Required(ErrorMessage = "El Sexo es requerido")]
+    [Display(Name = "Sexo")]
+    public char Sex { get; set; } = 'M';
+}
