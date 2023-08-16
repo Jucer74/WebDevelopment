97e4f931bd5046473235e0fb823d494c6ff363bb
arcethekiller <Penarandaalexander18@hotmail.com>
2023-08-15 21:25:07 -0500

    Explicacion de clase

---
+++ Practices/03.MVC/StudentsMVC/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsMVC.Models;
+
+public class Student
+{
+    [Key]
+    [Display(Name ="Id")]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "El nombre es requerido")]
+    [Display (Name = "Nombre")]
+    public string FirstName { get; set; } = null!;
+    [Required(ErrorMessage = "El apellido es requerido")]
+    public string LastName { get; set; } = null!;
+    [Required(ErrorMessage = "El Fecha es requerido")]
+    public DateTime DateOfBirth{ get; set; }
+    [Required(ErrorMessage = "El Fecha sexo es requerido")]
+    public char Sex { get; set; } = 'M';
+   
+}
+    
+    
+
