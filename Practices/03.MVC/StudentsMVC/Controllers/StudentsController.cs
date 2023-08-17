b4d9dbcf9e16b3f91cf51a1cc3bf6bf450573362
Sagudeloz <agudelosan2002@gmail.com>
2023-08-17 10:45:07 -0500

    a

---
+++ Practices/03.MVC/StudentsMVC/Controllers/StudentsController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentsMVC.Models;
+
+namespace StudentsMVC.Controllers
+{
+    public class StudentsController : Controller
+    {
+
+        // Global variable
+        private static List<Student> studentsList = LoadStudents();
+
+        // GET: StudentsController
+        public ActionResult Index()
+        {
+            return View(studentsList);
+        }
+
+        // GET: StudentsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: StudentsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #region Private-Methods
+
+        private static List<Student> LoadStudents()
+        {
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student() { Id = 1, FirstName = "John", LastName = "Doe", DateOfBirth = new DateTime(1980, 10, 10), Sex = 'M' });
+            students.Add(new Student() { Id = 2, FirstName = "Barry", LastName = "Allen", DateOfBirth = new DateTime(2001, 7, 7), Sex = 'M' });
+            students.Add(new Student() { Id = 3, FirstName = "Diana", LastName = "Prince", DateOfBirth = new DateTime(1950, 8, 8), Sex = 'F' });
+
+            return students;
+        }
+
+        #endregion Private-Methods
+
+    }
+
+}
---
+++ Practices/03.MVC/StudentsMVC/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsMVC.Models
+{
+    public class Student
+    {
+        [Key]
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [Display(Name = "Nombre")]
+        public string FirstName { get; set; } = null!;
+
+        [Required (ErrorMessage ="El apellido es requerido")]
+        public string LastName { get; set; } = null!;
+        [Required (ErrorMessage ="La fecha de nacimiento es requerida")]
+        public DateTime DateOfBirth { get; set; }
+        [Required (ErrorMessage ="El sexo es requerido")]
+        public char Sex { get; set; } = 'M';
+
+    }
+}
