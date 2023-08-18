9ae9df0b42a7ddd841a489a4c46c2e680dc8093d
FelipeAraujoG <felipeara04@gmail.com>
2023-08-17 20:21:17 -0500

    Boostrap
    primera pagina en boostrap

---
+++ Practices/03.MVC/students/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace students.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
