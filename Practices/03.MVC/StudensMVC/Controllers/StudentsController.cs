6784fc8c446d5a46b89bf7f758b4795cdb361713
Michael Estiven Reyes Escobar <maik1234reyes@gmail.com>
2023-08-19 17:24:23 -0500

    First-MVC-Code

---
+++ Practices/03.MVC/StudensMVC/Controllers/StudentsController.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudensMVC.Models;
+
+namespace StudensMVC.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private static List<Student> studentsList = LoadStudents();
+
+
+
+        // GET: StudentsController
+        public ActionResult Index()
+        {
+            return View(studentsList);
+        }
+
+        // GET: StudentsController/Details/5
+        public ActionResult Details(int id)
+        {
+            var student = studentsList.FirstOrDefault(student => x.Id == id);
+            return View();
+        }
+
+        // GET: StudentsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var student = studentsList.FirstOrDefault(x => x.Id == id);
+            return View();
+        }
+
+        // POST: StudentsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var student = studentsList.FirstOrDefault(x => x.Id == id);
+            return View();
+        }
+
+        // POST: StudentsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        private static  List<Student> LoadStudents()
+        {
+            var students = new List<Student>();
+
+            students.Add(new Student() {
+                Id=1, 
+                firstName="Carlos", 
+                lastName="Careverga",
+                DateOfBirth=new DateTime(2004,06,15), 
+                Sex='M'
+            });
+
+            students.Add(new Student()
+            {
+                Id = 2,
+                firstName = "Pedro",
+                lastName = "Careverga",
+                DateOfBirth = new DateTime(2004, 04, 15),
+                Sex = 'M'
+            });
+
+            students.Add(new Student()
+            {
+                Id = 3,
+                firstName = "Juan",
+                lastName = "Careverga",
+                DateOfBirth = new DateTime(2004, 02, 15),
+                Sex = 'M'
+            });
+
+            students.Add(new Student()
+            {
+                Id = 4,
+                firstName = "Camila",
+                lastName = "Careverga",
+                DateOfBirth = new DateTime(2004, 01, 15),
+                Sex = 'F'
+            });
+
+            return students;
+        }
+
+
+
+    }
+}
---
+++ Practices/03.MVC/StudensMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudensMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
