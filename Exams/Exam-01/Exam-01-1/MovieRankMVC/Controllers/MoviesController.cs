d67dbf50f26b1b2afc920ba1d4e1de4319127840
W-sebas23 <sebasmartinezr2014@gmail.com>
2023-08-22 20:21:09 -0500

    vistas de MoviesController
    vistas de MoviesController

---
+++ Exams/Exam-01/Exam-01-1/MovieRankMVC/Controllers/MoviesController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieRankMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: MoviesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: MoviesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MoviesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MoviesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MoviesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MoviesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MoviesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MoviesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
