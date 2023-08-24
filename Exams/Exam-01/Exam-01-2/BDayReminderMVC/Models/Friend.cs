eafd851ab132f2004739a4ac1ce202694b8ddd56
Julio Robles <Jucer74@Hotmail.com>
2023-08-24 16:40:39 -0500

    Update VErsion

---
+++ Exams/Exam-01/Exam-01-2/BDayReminderMVC/Models/Friend.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BDayReminderMVC.Models;
+
+public class Friend
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage ="The FirstName is Required")]
+    [StringLength(50)]
+    public string FirstName { get; set; } = null!;
+
+    [Required(ErrorMessage = "The LastName is Required")]
+    [StringLength(50)]
+    public string LastName { get; set; } = null!;
+
+    [Required(ErrorMessage = "The DateOfBirth is Required")]
+    [DataType(DataType.Date)]
+    public DateTime DateOfBirth { get; set; }
+
+    [Required(ErrorMessage = "The Sex is Required")]
+    [StringLength(1)]
+    public char Sex { get; set; } = 'M';
+}
