535d6bb2ce5b3d3483c3d677837fc763b69624d0
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2023-08-24 23:21:25 -0500

    update to view data
    see the data in the option to edit, delete and detail

--- Exams/Exam-01/Exam-01-1/MovieRankMVC/Controllers/MoviesController.cs
+++ Exams/Exam-01/Exam-01-1/MovieRankMVC/Controllers/MoviesController.cs
@@ -11,6 +11,23 @@ namespace MovieRankMVC.Controllers
         //global Variables
         private static List<Movie> moviesList = LoadMovies();
 
+        private static List<Movie> LoadMovies()
+        {
+            List<Movie> movies = new List<Movie>();
+
+            movies.Add(new Movie() { Id = 1, Title = "El Misterio en la Torre", Synopsis = "Intrigante película sobre un misterioso crimen en una antigua torre. Un detective debe resolver el enigma antes de que sea demasiado tarde.", Year = 2020, Duration = "2:00", Rate = 8, Poster = "misterio_torre.jpeg", Genres = "Misterio | Suspense" });
+
+            movies.Add(new Movie() { Id = 2, Title = "Reinos Olvidados", Synopsis = "Épica aventura en un mundo de fantasía donde un grupo de héroes debe unir fuerzas para salvar los reinos de la destrucción. Magia, monstruos y valentía se entrelazan en esta historia.", Year = 2015, Duration = "2:30", Rate = 9, Poster = "reinos_olvidados.jpeg", Genres = "Fantasía | Acción" });
+
+            movies.Add(new Movie() { Id = 3, Title = "El Último Viaje", Synopsis = "Un emocionante documental que sigue a un grupo de científicos mientras exploran las profundidades del océano en busca de criaturas desconocidas. Enfrentarán desafíos asombrosos en su búsqueda de descubrimientos.", Year = 2022, Duration = "1:45", Rate = 7, Poster = "ultimo_viaje.jpeg", Genres = "Documental | Aventura" });
+
+            movies.Add(new Movie() { Id = 4, Title = "El Enigma del Tiempo", Synopsis = "Un thriller de ciencia ficción que sigue a un viajero del tiempo accidental que debe desentrañar un complot para alterar el curso de la historia. Cada decisión cuenta mientras lucha por restaurar la línea temporal correcta.", Year = 2018, Duration = "2:10", Rate = 8, Poster = "enigma_tiempo.jpeg", Genres = "Ciencia Ficción | Suspense" });
+
+            movies.Add(new Movie() { Id = 5, Title = "El Legado del Dragón", Synopsis = "En un mundo donde los dragones y los humanos coexisten, un joven descubre un huevo de dragón en un antiguo bosque. A medida que la amistad entre ellos crece, se enfrentan a desafíos que pondrán a prueba su unión.", Year = 2010, Duration = "1:50", Rate = 7, Poster = "legado_dragon.jpeg", Genres = "Animación | Aventura" });
+
+            return movies;
+        }
+
         // GET: MoviesController
         public ActionResult Index()
         {
@@ -20,7 +37,8 @@ namespace MovieRankMVC.Controllers
         // GET: MoviesController/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var movie = moviesList.FirstOrDefault(x => x.Id == id);
+            return View(movie);
         }
 
         // GET: MoviesController/Create
@@ -47,7 +65,8 @@ namespace MovieRankMVC.Controllers
         // GET: MoviesController/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var movie = moviesList.FirstOrDefault(x => x.Id == id);
+            return View(movie);
         }
 
         // POST: MoviesController/Edit/5
@@ -68,7 +87,8 @@ namespace MovieRankMVC.Controllers
         // GET: MoviesController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var movie = moviesList.FirstOrDefault(x => x.Id == id);
+            return View(movie);
         }
 
         // POST: MoviesController/Delete/5
@@ -86,26 +106,6 @@ namespace MovieRankMVC.Controllers
             }
         }
 
-        #region Private-Methods
-
-        private static List<Movie> LoadMovies()
-        {
-            List<Movie> movies = new List<Movie>();
-
-            movies.Add(new Movie() { Id = 1, Title = "El Misterio en la Torre", Synopsis = "Intrigante película sobre un misterioso crimen en una antigua torre. Un detective debe resolver el enigma antes de que sea demasiado tarde.", Year = 2020, Duration = "2:00", Rate = 8, Poster = "misterio_torre.jpeg", Genres = "Misterio | Suspense" });
-
-            movies.Add(new Movie() { Id = 2, Title = "Reinos Olvidados", Synopsis = "Épica aventura en un mundo de fantasía donde un grupo de héroes debe unir fuerzas para salvar los reinos de la destrucción. Magia, monstruos y valentía se entrelazan en esta historia.", Year = 2015, Duration = "2:30", Rate = 9, Poster = "reinos_olvidados.jpeg", Genres = "Fantasía | Acción" });
-
-            movies.Add(new Movie() { Id = 3, Title = "El Último Viaje", Synopsis = "Un emocionante documental que sigue a un grupo de científicos mientras exploran las profundidades del océano en busca de criaturas desconocidas. Enfrentarán desafíos asombrosos en su búsqueda de descubrimientos.", Year = 2022, Duration = "1:45", Rate = 7, Poster = "ultimo_viaje.jpeg", Genres = "Documental | Aventura" });
-
-            movies.Add(new Movie() { Id = 4, Title = "El Enigma del Tiempo", Synopsis = "Un thriller de ciencia ficción que sigue a un viajero del tiempo accidental que debe desentrañar un complot para alterar el curso de la historia. Cada decisión cuenta mientras lucha por restaurar la línea temporal correcta.", Year = 2018, Duration = "2:10", Rate = 8, Poster = "enigma_tiempo.jpeg", Genres = "Ciencia Ficción | Suspense" });
-
-            movies.Add(new Movie() { Id = 5, Title = "El Legado del Dragón", Synopsis = "En un mundo donde los dragones y los humanos coexisten, un joven descubre un huevo de dragón en un antiguo bosque. A medida que la amistad entre ellos crece, se enfrentan a desafíos que pondrán a prueba su unión.", Year = 2010, Duration = "1:50", Rate = 7, Poster = "legado_dragon.jpeg", Genres = "Animación | Aventura" });
-
-            return movies;
-        }
-
-        #endregion Private-Methods
 
     }
 }
