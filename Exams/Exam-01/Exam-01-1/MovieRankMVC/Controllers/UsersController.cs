209fdff918b81bb0e985bd326195db3059350eb3
Abc <bjulian127@gmail.com>
2023-08-26 17:34:09 -0500

    Version 1.0
    crud basico sin personalizar y sin nad

---
+++ Exams/Exam-01/Exam-01-1/MovieRankMVC/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieRankMVC.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: UsersController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: UsersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+    }
+}
