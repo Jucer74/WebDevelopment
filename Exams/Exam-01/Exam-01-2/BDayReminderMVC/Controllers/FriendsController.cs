2577446a3a3d2a6a773bc39a4a4d66494989b9bb
Michael Estiven Reyes Escobar <maik1234reyes@gmail.com>
2023-08-26 18:27:50 -0500

    Controller-Class-started

---
+++ Exams/Exam-01/Exam-01-2/BDayReminderMVC/Controllers/FriendsController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BDayReminderMVC.Controllers
+{
+    public class FriendsController : Controller
+    {
+        // GET: FriendsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: FriendsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: FriendsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FriendsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FriendsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: FriendsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: FriendsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: FriendsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Exams/Exam-01/Exam-01-2/BDayReminderMVC/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BDayReminderMVC.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The FirstName is Required")]
+        [StringLength(50)]
+        public string firstName { get; set; }
+
+        [Required(ErrorMessage = "The LastName is Required")]
+        [StringLength(50)]
+        public string lastName { get; set; }
+
+        [Required(ErrorMessage = "The Email is Required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The Password is Required")]
+        [StringLength(50)]
+        public string Password { get; set; }
+
+
+    }
+}
