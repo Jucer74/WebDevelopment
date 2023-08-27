10aeae904bd4a21d2e93dc7dfd94802379002f37
W-sebas23 <sebasmartinezr2014@gmail.com>
2023-08-27 18:46:19 -0500

    users register

--- Exams/Exam-01/Exam-01-1/MovieRankMVC/Controllers/UsersController.cs
+++ Exams/Exam-01/Exam-01-1/MovieRankMVC/Controllers/UsersController.cs
@@ -1,17 +1,33 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MovieRankMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MovieRankMVC.Controllers
 {
     public class UsersController : Controller
     {
+
+        private static List<User> usersList = LoadUsers();
+
+
+        private static List<User> LoadUsers()
+        {
+            List<User> users = new List<User>();
+
+            users.Add(new User() { Id = 1, UserEmail = "admin@email.com", FirstName = "Admin", LastName = "User", Password = "P4ssw0rd*01" });
+
+            return users;
+        }
+
+
         // GET: UsersController
         public ActionResult Index()
         {
             return View();
         }
 
-       
         // GET: UsersController/Create
         public ActionResult Create()
         {
@@ -21,10 +37,15 @@ namespace MovieRankMVC.Controllers
         // POST: UsersController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(User user)
         {
             try
             {
+                int newId = usersList.Count + 1;
+                user.Id = newId;
+
+                usersList.Add(user);
+
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -33,14 +54,56 @@ namespace MovieRankMVC.Controllers
             }
         }
 
+        // GET: UsersController/Login
         public IActionResult Login()
         {
             return View();
         }
 
+        // POST: UsersController/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(User user)
+        {
+            var existingUser = usersList.FirstOrDefault(u => u.UserEmail == user.UserEmail && u.Password == user.Password);
+
+            if (existingUser != null)
+            {
+                // Usuario autenticado, redirigir a la página de inicio
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // Usuario no encontrado o contraseña incorrecta, mostrar mensaje de error
+                ModelState.AddModelError("", "Email or password is incorrect");
+                return View();
+            }
+        }
+
+        // GET: UsersController/Register
         public IActionResult Register()
         {
             return View();
+        }
+
+        // POST: UsersController/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(User user)
+        {
+            try
+            {
+                int newId = usersList.Count + 1;
+                user.Id = newId;
+
+                usersList.Add(user);
+
+                return RedirectToAction(nameof(Login));
+            }
+            catch
+            {
+                return View();
+            }
         }
 
     }
