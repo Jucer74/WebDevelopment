e7c12da04933515301f45bd0ba3064eb9320895f
juanse1229 <111519122+juanse1229@users.noreply.github.com>
2023-08-28 17:38:47 -0500

    crud

---
+++ Exams/Exam-01/Exam-01-2/BDayReminderMVC/Controllers/FriendsController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BDayReminderMVC.Controllers
+{
+    public class FriendsController : Controller
+    {
+        // GET: Friend
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Friend/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Friend/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Friend/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Friend/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Friend/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Friend/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Friend/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
