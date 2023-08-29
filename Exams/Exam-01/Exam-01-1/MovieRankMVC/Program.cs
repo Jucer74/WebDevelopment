981f2e5f2c7aefe9ae912db18aebfaa343bd42ea
sntgrstrp <107089380+sntgrstrp@users.noreply.github.com>
2023-08-29 18:03:02 -0500

    Exam 1 v1

--- Exams/Exam-01/Exam-01-1/MovieRankMVC/Program.cs
+++ Exams/Exam-01/Exam-01-1/MovieRankMVC/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Users}/{action=Login}/{id?}");
 
 app.Run();
