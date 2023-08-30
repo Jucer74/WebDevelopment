f9002712583a0536c5465ab6893bf3d323796247
JJMUNOZB <jjmunozb@correo.usbcali.edu.co>
2023-08-30 17:38:21 -0500

    M

--- Exams/Exam-01/Exam-01-2/BDayReminderMVC/Program.cs
+++ Exams/Exam-01/Exam-01-2/BDayReminderMVC/Program.cs
@@ -22,7 +22,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Users}/{action=Login}/{id?}");
 
 
 
