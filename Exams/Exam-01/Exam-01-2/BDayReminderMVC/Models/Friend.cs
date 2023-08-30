72d131943b9a3264442f44d299c985615945cb6e
nostcoper <carlosserrato200475@gmail.com>
2023-08-29 22:12:22 -0500

    arreglos de modelo

--- Exams/Exam-01/Exam-01-2/BDayReminderMVC/Models/Friend.cs
+++ Exams/Exam-01/Exam-01-2/BDayReminderMVC/Models/Friend.cs
@@ -7,25 +7,30 @@ public class Friend
     [Key]
     public int Id { get; set; }
 
-    [Required(ErrorMessage ="The FirstName is Required")]
+    [Required(ErrorMessage = "The FirstName is Required")]
     [StringLength(50)]
+    [Display(Name = "First Name")]
     public string FirstName { get; set; } = null!;
 
     [Required(ErrorMessage = "The LastName is Required")]
     [StringLength(50)]
+    [Display(Name = "Last Name")]
     public string LastName { get; set; } = null!;
 
     [Required(ErrorMessage = "The DateOfBirth is Required")]
     [DataType(DataType.Date)]
+    [Display(Name = "Date Of Birth")]
     public DateTime DateOfBirth { get; set; }
 
-    [Required(ErrorMessage = "The Sex is Required")]
-    [StringLength(1)]
-    public char Sex { get; set; } = 'M';
-    
-    public string Relations { get; set; } = null!;    
-    
+    [Required(ErrorMessage = "The Gender is Required")]
+    [Display(Name = "Gender")]
+    public char Gender { get; set; } = 'M';
+
+    [Display(Name = "Relations")]
+    public List<string> Relations { get; set; } = null!;
+
     [Required(ErrorMessage = "The Photo is required")]
+    [Display(Name = "Photo")]
     [StringLength(50)]
     // Include only ImageName.ext
     public string Photo { get; set; } = null!;    
