e910d97b882cac3eb9114475cf7733108eb56de4
Juan Hurtado <jfhvjfhv0015@gmail.com>
2023-08-29 23:30:13 -0500

    Subida de segunda correcion

--- Exams/Exam-01/Exam-01-1/MovieRankMVC/Models/User.cs
+++ Exams/Exam-01/Exam-01-1/MovieRankMVC/Models/User.cs
@@ -7,7 +7,7 @@ namespace MovieRankMVC.Models
         [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "The UserEmail is required")]
+        [Required(ErrorMessage = "The Email is required")]
         [EmailAddress(ErrorMessage = "The UserEmail must be a valid email address")]
         [StringLength(300)]
         public string UserEmail { get; set; } = null!;
@@ -20,10 +20,11 @@ namespace MovieRankMVC.Models
         [StringLength(50)]
         public string LastName { get; set; } = null!;
 
-        [Required(ErrorMessage = "The Password is required")]
-        [DataType(DataType.Password)]
-        [StringLength(50)]
-        public string Password { get; set; } = null!;
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
+        public string? Password { get; set; }
+ 
 
         [Compare("Password", ErrorMessage = "Please confirm your password")]
         [DataType(DataType.Password)]
