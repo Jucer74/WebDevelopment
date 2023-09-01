56b8fc12b99cfa725045895271e01bcf79225379
Luhy Loas <luysnickers@gmail.com>
2023-08-31 19:00:29 -0500

    k

---
+++ Exams/Exam-01/Exam-01-2/Parcial/Controllers/Register.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Parcial.Controllers
+{
+    public class Register : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ Exams/Exam-01/Exam-01-2/Parcial/Models/Persona.cs
@@ -0,0 +1,12 @@
+﻿namespace Parcial.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string ImagenPath { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+
+}
