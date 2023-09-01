d58307b807b48bb4badcf4d361d647cceea6d5f3
ElMecchi <matmed009@gmail.com>
2023-08-31 23:42:49 -0500

    Parcial

--- Exams/Exam-01/Exam-01-2/Parcial/wwwroot/js/BdayCalculator.js
+++ Exams/Exam-01/Exam-01-2/Parcial/wwwroot/js/BdayCalculator.js
@@ -1,19 +1,18 @@
-﻿// BdayCalculator.js
-document.addEventListener("DOMContentLoaded", function () {
+﻿document.addEventListener("DOMContentLoaded", function () {
     var today = new Date();
     var daysRemainingElements = document.querySelectorAll(".days-remaining-placeholder");
 
     daysRemainingElements.forEach(function (element) {
-        var birthday = new Date(element.getAttribute("data-birthday"));
+        var birthdayString = element.getAttribute("data-birthday"); // Obtener la fecha de cumpleaños en formato de cadena
+        var birthday = new Date(birthdayString);
         birthday.setFullYear(today.getFullYear()); // Ajustar el año para este año
+
         if (birthday < today) {
-            birthday.setFullYear(today.getFullYear() + 1); // Si ya pasó, ajustar al próximo año
+            // Si ya pasó, ajustar al próximo año
+            birthday.setFullYear(today.getFullYear() + 1);
         }
 
         var daysRemaining = Math.ceil((birthday - today) / (1000 * 60 * 60 * 24));
         element.textContent = daysRemaining + " días restantes para el cumpleaños";
-
-
-        
     });
 });
