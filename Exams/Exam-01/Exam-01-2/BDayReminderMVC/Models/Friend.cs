7410516ef2017e5861ed2e425842e69aa3664e1d
Michael Estiven Reyes Escobar <maik1234reyes@gmail.com>
2023-08-31 19:02:19 -0500

    Complete-Parcial

---
+++ Exams/Exam-01/Exam-01-2/BDayReminderMVC/Models/Friend.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BDayReminderMVC.Models;
+
+public class Friend
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "The FirstName is Required")]
+    [StringLength(50)]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; } = null!;
+
+    [Required(ErrorMessage = "The LastName is Required")]
+    [StringLength(50)]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; } = null!;
+
+    [Required(ErrorMessage = "The DateOfBirth is Required")]
+    [DataType(DataType.Date)]
+    [Display(Name = "Date Of Birth")]
+    public DateOnly DateOfBirth { get; set; }
+
+    [Required(ErrorMessage = "The Gender is Required")]
+    [Display(Name = "Gender")]
+    public char Gender { get; set; } = 'M';
+
+    [Display(Name = "Relations")]
+    public List<string> Relations { get; set; } = null!;
+
+    [Required(ErrorMessage = "The Photo is required")]
+    [StringLength(50)]
+    [Display(Name = "Photo")]
+    // Include only ImageName.ext
+    public string Photo { get; set; } = null!;    
+}
---
+++ Exams/Exam-01/Exam-01-2/BDayReminderMVC/Models/User.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BDayReminderMVC.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The FirstName is Required")]
+        [StringLength(50)]
+        public string FirstName { get; set; } = null!;
+
+        [Required(ErrorMessage = "The LastName is Required")]
+        [StringLength(50)]
+        public string LastName { get; set; } = null!;
+
+        [Required(ErrorMessage = "The Email is Required")]
+        [DataType(DataType.EmailAddress)]
+        public string UserName { get; set; } = null!;
+
+        [Required(ErrorMessage = "The Password is Required")]
+        [StringLength(50)]
+        public string Password { get; set; } = null!;
+
+        [Required(ErrorMessage = "The ConfirmPassword is Required")]
+        [Compare("Password", ErrorMessage = "The Confirm Password does not match.")]
+        [StringLength(50)]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
