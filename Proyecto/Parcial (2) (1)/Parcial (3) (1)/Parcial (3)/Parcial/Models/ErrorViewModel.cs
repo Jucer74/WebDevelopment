77551cb9e028d7748e4876f7f3dedd486052d01a
ElMecchi <matmed009@gmail.com>
2023-08-31 19:12:34 -0500

    .

--- Proyecto/Parcial (2) (1)/Parcial (3) (1)/Parcial (3)/Parcial/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Parcial.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
