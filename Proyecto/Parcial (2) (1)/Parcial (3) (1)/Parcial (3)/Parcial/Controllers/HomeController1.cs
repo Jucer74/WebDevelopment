247c584103a005c8a47cfda895f1f444de79371c
ElMecchi <matmed009@gmail.com>
2023-08-31 19:03:42 -0500

    Parcial

---
+++ Proyecto/Parcial (2) (1)/Parcial (3) (1)/Parcial (3)/Parcial/Controllers/HomeController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Parcial.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
