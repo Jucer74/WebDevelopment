32e935a5a3dfade2dea8c0b06c5314b427deaa23
W-sebas23 <sebasmartinezr2014@gmail.com>
2023-09-12 19:28:53 -0500

    mvc services

---
+++ Practices/04.WebAPI/StudentsMVC/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsMVC.Models;
+
+public class Student
+{
+    [Key]
+    [Display(Name = "Id")]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "El nombre es requerido")]
+    [Display(Name = "Nombre")] //le asigna un nombre diferentea al de
+                               //la variable de la clase
+                               //fisrtname cambia a Nombre en el display
+    public string FirstName { get; set; } = null!;
+
+    [Required(ErrorMessage = "El apellido es requerido")]
+    [Display(Name = "Apellido")]
+    public string LastName { get; set; } = null!;
+
+    [Required(ErrorMessage = "El cumpleaños es requerido")]
+    [Display(Name = "Cumpleaños")]
+    public DateTime DateOfBirth { get; set; }
+
+    [Required(ErrorMessage = "El sexo es requerido")]
+    [Display(Name = "Sexo")]
+    public char Sex { get; set; } = 'M';
+    
+
+    
+}
