6aaf105163b5a71090bcbe701d218faf73f74eeb
Abc <bjulian127@gmail.com>
2023-09-12 19:32:16 -0500

    .

--- Practices/03.MVC/StudentsMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StudentsMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
