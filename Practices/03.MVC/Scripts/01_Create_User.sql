5e28703c2a844d2b29bc03c88b4de573f67cd8c8
Julio Robles <Jucer74@Hotmail.com>
2023-09-14 21:03:30 -0500

    Update 01_Create_User.sql

--- Practices/03.MVC/Scripts/01_Create_User.sql
+++ Practices/03.MVC/Scripts/01_Create_User.sql
@@ -1,4 +1,4 @@
 /*DROP USER 'schooluser'@'localhost' ;*/
-CREATE USER 'schooluser'@'localhost' IDENTIFIED BY 'Sch00lUs3*01';
+CREATE USER 'schooluser'@'localhost' IDENTIFIED BY 'Sch00lUs3r*01';
 GRANT ALL PRIVILEGES ON *.* TO 'schooluser'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
