1e94d361141f1ba413c2bd90fbc7b4190a3cb33b
Julio Robles <Jucer74@Hotmail.com>
2023-09-28 20:29:38 -0500

    Update WebApi

---
+++ Practices/04.WebAPI/StudentsMVC/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentsMVC.Models;
+
+namespace StudentsMVC.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
