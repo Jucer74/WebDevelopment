cc2de853c3a7850abb8d0b8c04fefa204f67f791
MaxElVegano <aidin2210@gmail.com>
2023-10-02 21:56:20 -0500

    Update MaxSqlContext.cs

--- Practices/03.MVC/Crud/Models/MaxSqlContext.cs
+++ Practices/03.MVC/Crud/Models/MaxSqlContext.cs
@@ -18,8 +18,8 @@ public partial class MaxSqlContext : DbContext
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;port=3306;database=max_sql;uid=root;password=Un1c0rn10", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.34-mysql"));
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        //=> optionsBuilder.UseMySql("server=localhost;port=3306;database=max_sql;uid=root;password=Un1c0rn10", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.34-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
