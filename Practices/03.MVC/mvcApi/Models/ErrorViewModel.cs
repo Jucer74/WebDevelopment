cda5fb0f9314c4f073698c8abcaaf68237adb3fb
Juanjo271 <juanop556@hotmail.com>
2023-10-03 19:09:36 -0500

    parcial

---
+++ Practices/03.MVC/mvcApi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvcApi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
