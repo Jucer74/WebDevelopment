3a897c9b877d6f971d71698185e0193f0ee7a4b8
arcethekiller <100455711+arcethekiller@users.noreply.github.com>
2023-10-04 18:20:21 -0500

    Update Program.cs

--- Exams/Exam-02/Exam-02-1/Code/MoneyBankMVC/Program.cs
+++ Exams/Exam-02/Exam-02-1/Code/MoneyBankMVC/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using MoneyBankMVC.Context;
-using MoneyBankMVC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,8 +11,6 @@ builder.Services.AddDbContext<MoneybankdbContext>(options =>
     new MySqlServerVersion(new Version(8, 0, 21)))
 );
 
-// Agregar el servicio IAccountService con alcance (scoped)
-builder.Services.AddScoped<IAccountService, AccountService>();
 
 var app = builder.Build();
 
