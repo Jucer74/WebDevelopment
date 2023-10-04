88d73b055c0c2278474fbe35c5fc5b6379087390
MJ <>
2023-10-03 21:03:04 -0500

    V1

---
+++ Exams/Exam-02/Exam-02-1/Code/MoneyBankMVC/Context/MoneybankdbContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using MoneyBankMVC.Models;
+
+namespace MoneyBankMVC.Context;
+
+public partial class MoneybankdbContext : DbContext
+{
+    public MoneybankdbContext()
+    {
+    }
+
+    public MoneybankdbContext(DbContextOptions<MoneybankdbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Account> Accounts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=localhost;database=moneybankdb;user id=moneybankuser;password=M0n3yB4nkUs3r*01", ServerVersion.Parse("8.0.33-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Account>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("accounts");
+
+            entity.Property(e => e.AccountNumber).HasMaxLength(10);
+            entity.Property(e => e.AccountType).HasMaxLength(1);
+            entity.Property(e => e.BalanceAmount).HasPrecision(18, 2);
+            entity.Property(e => e.CreationDate).HasColumnType("datetime");
+            entity.Property(e => e.OverdraftAmount).HasPrecision(18, 2);
+            entity.Property(e => e.OwnerName).HasMaxLength(100);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Exams/Exam-02/Exam-02-1/Code/MoneyBankMVC/Models/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace MoneyBankMVC.Models
+{
+    public partial class Account
+    {
+        [Key]
+        [Required(ErrorMessage = "El Id es requerido")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DisplayName("Id")]
+        public int Id { get; set; }
+
+        [StringLength(1)]
+        [RegularExpression("^[AC]$", ErrorMessage = "Tipo de cuenta solo permite 'A' o 'C'.")]
+        [Required(ErrorMessage = "El tipo de cuenta es requerido")]
+        [DisplayName("Tipo de Cuenta")]
+        public string AccountType { get; set; } = null!;
+
+        [Required(ErrorMessage = "La fecha de creación es requerida")]
+        [DisplayName("Fecha de Creación")]
+        public DateTime CreationDate { get; set; }
+
+        [Required(ErrorMessage = "El número de cuenta es requerido")]
+        [DisplayName("Número de Cuenta")]
+        public string AccountNumber { get; set; } = null!;
+
+        [Required(ErrorMessage = "El nombre del propietario es requerido")]
+        [DisplayName("Nombre del Propietario")]
+        public string OwnerName { get; set; } = null!;
+
+        [Required(ErrorMessage = "El saldo es requerido")]
+        [DisplayName("Saldo")]
+        public decimal BalanceAmount { get; set; }
+
+        [DisplayName("Monto de Sobregiro")]
+        public decimal OverdraftAmount { get; set; }
+    }
+}
