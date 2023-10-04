de5ba7b3d0309c7b955e49fe9786648da077ece9
Abc <bjulian127@gmail.com>
2023-10-04 11:57:38 -0500

    ultima version sin warnings

--- Exams/Exam-02/Exam-02-1/Code/MoneyBankMVC/Context/MoneybankdbContext.cs
+++ Exams/Exam-02/Exam-02-1/Code/MoneyBankMVC/Context/MoneybankdbContext.cs
@@ -3,50 +3,45 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using MoneyBankMVC.Models;
 
-namespace MoneyBankMVC.Context;
-
-public partial class MoneybankdbContext : DbContext
+namespace MoneyBankMVC.Context
 {
-    public MoneybankdbContext()
+    public partial class MoneybankdbContext : DbContext
     {
-    }
-
-    public MoneybankdbContext(DbContextOptions<MoneybankdbContext> options)
-        : base(options)
-    {
-    }
-
-    public virtual DbSet<Account> Accounts { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-
-
-    }
-
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder
-            .UseCollation("utf8mb4_0900_ai_ci")
-            .HasCharSet("utf8mb4");
-
-        modelBuilder.Entity<Account>(entity =>
+        public MoneybankdbContext()
         {
-            entity.HasKey(e => e.Id).HasName("PRIMARY");
+        }
 
-            entity.ToTable("accounts");
+        public MoneybankdbContext(DbContextOptions<MoneybankdbContext> options)
+            : base(options)
+        {
+        }
 
-            entity.Property(e => e.AccountNumber).HasMaxLength(10);
-            entity.Property(e => e.AccountType).HasMaxLength(1);
-            entity.Property(e => e.BalanceAmount).HasPrecision(18, 2);
-            entity.Property(e => e.CreationDate).HasColumnType("datetime");
-            entity.Property(e => e.OverdraftAmount).HasPrecision(18, 2);
-            entity.Property(e => e.OwnerName).HasMaxLength(100);
-        });
+        public virtual DbSet<Account> Accounts { get; set; }
 
-        OnModelCreatingPartial(modelBuilder);
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Deja este método vacío si no necesitas configurar la base de datos aquí.
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .UseCollation("utf8mb4_0900_ai_ci")
+                .HasCharSet("utf8mb4");
+
+            modelBuilder.Entity<Account>(entity =>
+            {
+                entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+                entity.ToTable("accounts");
+
+                entity.Property(e => e.AccountNumber).HasMaxLength(10);
+                entity.Property(e => e.AccountType).HasMaxLength(1);
+                entity.Property(e => e.BalanceAmount).HasPrecision(18, 2);
+                entity.Property(e => e.CreationDate).HasColumnType("datetime");
+                entity.Property(e => e.OverdraftAmount).HasPrecision(18, 2);
+                entity.Property(e => e.OwnerName).HasMaxLength(100);
+            });
+        }
     }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
