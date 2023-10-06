b7486e52345ddbe71ac35d656222d21524126ef9
JJMUNOZB <jjmunozb@correo.usbcali.edu.co>
2023-10-05 22:41:26 -0500

    mm

--- Exams/Exam-02/Exam-02-2/Code/MoneyBankMVC/Controllers/HomeController.cs
+++ Exams/Exam-02/Exam-02-2/Code/MoneyBankMVC/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ namespace MoneyBankMVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+ 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
