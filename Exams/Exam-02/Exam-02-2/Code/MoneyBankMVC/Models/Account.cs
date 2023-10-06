2170266d581634be960a74a54b5f6c71c4998fb2
httpsmarioooo <111519152+httpsmarioooo@users.noreply.github.com>
2023-10-06 02:10:35 -0500

    Botones y Titulos de la "Tabla"

--- Exams/Exam-02/Exam-02-2/Code/MoneyBankMVC/Models/Account.cs
+++ Exams/Exam-02/Exam-02-2/Code/MoneyBankMVC/Models/Account.cs
@@ -7,27 +7,33 @@ namespace MoneyBankMVC.Models
 
         [Key]
         public int Id { get; set; }
+        [Display(Name ="Tipo")]
 
         [Required(ErrorMessage = "(El campo Tipo de cuenta es requerido)")]
         [RegularExpression("[AC]",ErrorMessage ="El campo tipo de cuenta solo permite (A O C)")]
 
         public char AccountType { get; set; } = 'A';
 
+
+        [Display(Name = "Fecha")]
         [DataType(DataType.Date)]
         public DateTime CreationDate { get; set; } = DateTime.Now;
 
-
-        [Required(ErrorMessage ="El campo numero de cuenta es requerido")]
-        [MaxLength(10,ErrorMessage = "El campo Numero de La Cuenta tiene una longitud maxima de 10 caracteres")]
+        [Display(Name = "Cuenta")]
+        [Required(ErrorMessage = "El campo numero de cuenta es requerido")]
+        [MaxLength(10, ErrorMessage = "El campo Numero de La Cuenta tiene una longitud maxima de 10 caracteres")]
         [RegularExpression(@"^\d{10}", ErrorMessage = "El campo Numero de la Cuenta Solo Acepta Numeros")]
         public string AccountNumber { get; set; } = null!;
 
 
+
+        [Display(Name = "Nombre")]
         [Required(ErrorMessage ="El campo nombre del probetiario es requqerido")]
         [MaxLength(100, ErrorMessage = "El campo nombre de propietario tiene una longitud maxima de 100 caracteres")]
         public string OwnerName { get; set; } = null!;
 
 
+        [Display(Name = "Balance")]
         [Required(ErrorMessage = "El campo Balance es Requerido")]
         [RegularExpression(@"^\d+.?\d{0,2}$", ErrorMessage = "El campo Balance debe ser en formato Moneda (0.00)")]
         public decimal BalanceAmount { get; set; }
