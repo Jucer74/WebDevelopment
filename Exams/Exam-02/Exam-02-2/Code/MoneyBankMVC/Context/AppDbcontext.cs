becb4d09f6a94e4323ba7ac1f760451caa3370c7
juanzv505 <111458745+juanzv505@users.noreply.github.com>
2023-10-05 21:22:34 -0500

    parcial

---
+++ Exams/Exam-02/Exam-02-2/Code/MoneyBankMVC/Context/AppDbcontext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyBankMVC.Models;
+using System.Collections.Generic;
+
+namespace MoneyBankMVC.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
