fc749f1afbd6799ea3c64e222fa766a480088969
juanzv505 <juanjosezunigaveraz@gmail.com>
2023-10-07 10:37:38 -0500

    parcial web
    -

--- Exams/Exam-02/Exam-02-2/Code/MoneyBankMVC/Program.cs
+++ Exams/Exam-02/Exam-02-2/Code/MoneyBankMVC/Program.cs
@@ -1,4 +1,3 @@
-
 using Microsoft.EntityFrameworkCore;
 using MoneyBankMVC.Context;
 
@@ -7,19 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add DBContext
 builder.Services.AddDbContext<AppDbContext>(options => options.UseMySQL(builder.Configuration.GetConnectionString("CnnStr")!));
 
-
-var builder = WebApplication.CreateBuilder(args);
-
- main
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 
-
-
- main
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
