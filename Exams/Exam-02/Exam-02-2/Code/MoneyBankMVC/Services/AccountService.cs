b8c758d008dfd9f3845ad1ec95aa9ae22c134907
ElMecchi <matmed009@gmail.com>
2023-10-07 00:30:46 -0500

    EF

--- Exams/Exam-02/Exam-02-2/Code/MoneyBankMVC/Services/AccountService.cs
+++ Exams/Exam-02/Exam-02-2/Code/MoneyBankMVC/Services/AccountService.cs
@@ -1,5 +1,112 @@
 ﻿namespace MoneyBankMVC.Services;
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MoneyBankMVC.Context;
+using MoneyBankMVC.Models;
+
 public class AccountService : IAccountService
 {
+    private readonly AppDbContext _context;
+
+    public AccountService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Account>> GetAccountsAsync()
+    {
+        return await _context.Accounts.ToListAsync();
+    }
+
+    public async Task<Account> GetAccountByIdAsync(int id)
+    {
+        return await _context.Accounts.FirstOrDefaultAsync(a => a.Id == id);
+    }
+
+    public async Task<bool> CreateAccountAsync(Account account)
+    {
+        try
+        {
+            _context.Accounts.Add(account);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception)
+        {
+            // Manejo de errores
+            return false;
+        }
+    }
+
+    public async Task<bool> UpdateAccountAsync(Account account)
+    {
+        try
+        {
+            _context.Update(account);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception)
+        {
+            // Manejo de errores
+            return false;
+        }
+    }
+
+    public async Task<bool> DeleteAccountAsync(int id)
+    {
+        try
+        {
+            var account = await _context.Accounts.FindAsync(id);
+            if (account != null)
+            {
+                _context.Accounts.Remove(account);
+                await _context.SaveChangesAsync();
+            }
+            return true;
+        }
+        catch (Exception)
+        {
+            // Manejo de errores
+            return false;
+        }
+    }
+
+    public async Task<bool> DepositAsync(Transaction transaction)
+    {
+        try
+        {
+            // Lógica para depositar dinero en la cuenta
+            // Actualiza el saldo de la cuenta y guarda en la base de datos
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception)
+        {
+            // Manejo de errores
+            return false;
+        }
+    }
+
+    public async Task<bool> WithdrawAsync(Transaction transaction)
+    {
+        try
+        {
+            // Lógica para realizar un retiro de la cuenta
+
+            await _context.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception)
+        {
+            // Manejo de errores
+            return false;
+        }
+    }
 }
+
