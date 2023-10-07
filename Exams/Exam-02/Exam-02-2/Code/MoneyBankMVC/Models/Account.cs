788f1252475f425c5636c930bf4acedcf8ea4c90
FelipeAraujoG <felipeara04@gmail.com>
2023-10-07 11:12:04 -0500

    parcial

--- Exams/Exam-02/Exam-02-2/Code/MoneyBankMVC/Models/Account.cs
+++ Exams/Exam-02/Exam-02-2/Code/MoneyBankMVC/Models/Account.cs
@@ -9,28 +9,36 @@ namespace MoneyBankMVC.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "El campo TIPO DE CUENTA es Requerido")]
-        [RegularExpression("[AC]", ErrorMessage = "El campo TIPO DE CUENTA solo permite (A o C)")]
+
+        [Display(Name = "Tipo")]
+        [Required(ErrorMessage = "El campo Tipo de Cuenta es Requerido)")]
+        [RegularExpression("[AC]", ErrorMessage = "El campo Tipo de Cuenta solo permite (A o C)")]
         public char AccountType { get; set; } = 'A';
 
+        [Display(Name = "Fecha")]
         [DataType(DataType.Date)]
         public DateTime CreationDate { get; set; } = DateTime.Now;
 
-        [Required(ErrorMessage = "El campo NUMERO DE CUENTA es Requerido")]
-        [MaxLength(10, ErrorMessage = "El campo NUMERO DE CUENTA tiene un maximo de caracteres es 10")]
-        [RegularExpression(@"\d{10}", ErrorMessage = "El campo NUMERO DE CUENTA solo acepta numeros")]
+        [Display(Name = "No. Cuenta")]
+        [Required(ErrorMessage = "El campo Numero de la Cuenta es Requerido")]
+        [MaxLength(10, ErrorMessage = "El campo Numero de La Cuenta tiene una longitud maxima de 10 caracteres")]
+        [RegularExpression(@"\d{10}", ErrorMessage = "El campo Numero de la Cuenta Sólo Acepta Numeros")]
         public string AccountNumber { get; set; } = null!;
 
-        [Required(ErrorMessage = "El campo NOMBRE DEL PROPIETARIO es Requerido")]
-        [MaxLength(100, ErrorMessage = "El campo NOMBRE DEL PROPIETARIO tiene un maximo de caracteres es 100")]
+        [Display(Name = "Nombre")]
+        [Required(ErrorMessage = "El campo Nombre del Propietario es Requerido")]
+        [MaxLength(100, ErrorMessage = "El campo Nombre del Propietario tiene una longitud maxima de 100 caracteres")]
         public string OwnerName { get; set; } = null!;
 
-        [Required(ErrorMessage = "El campo BALANCE es Requerido")]
-        [RegularExpression(@"^\d+.?\d{0,2}$", ErrorMessage = "El campo BALANCE es debe ser en formato moneda (0.00)")]
+        [Display(Name = "Balance")]
+        [Required(ErrorMessage = "El campo Balance es Requerido")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El Monto del Balance debe ser mayor a cero.")]
+        [DataType(DataType.Currency)]
         public decimal BalanceAmount { get; set; }
 
-        [Required(ErrorMessage = "El campo SOBREGIRO es Requerido")]
-        [RegularExpression(@"^\d+.?\d{0,2}$", ErrorMessage = "El campo SOBREGIRO es debe ser en formato moneda (0.00)")]
+        [Display(Name = "Sobregiro")]
+        [Required(ErrorMessage = "El campo Sobregiro es Requerido")]
+        [DataType(DataType.Currency)]
         public decimal OverdraftAmount { get; set; }
     }
 }
