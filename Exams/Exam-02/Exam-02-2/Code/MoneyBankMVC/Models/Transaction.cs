4c1072be95136304315f863b9b088b736d1bdaa8
YesidBCO <111519452+YesidBCO@users.noreply.github.com>
2023-10-06 23:07:49 -0500

    Avance

--- Exams/Exam-02/Exam-02-2/Code/MoneyBankMVC/Models/Transaction.cs
+++ Exams/Exam-02/Exam-02-2/Code/MoneyBankMVC/Models/Transaction.cs
@@ -1,7 +1,12 @@
-﻿namespace MoneyBankMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoneyBankMVC.Models
 {
     public class Transaction
     {
-
+        [Display(Name = "Valor")]
+        [Required(ErrorMessage = "El campo V alor es Requerido")]
+        [RegularExpression(@"^\d+.?\d{0,2}$", ErrorMessage = "El campo Valor debe ser en formato Moneda (0.00)")]
+        public decimal ValueAmount { get; set; }
     }
 }
