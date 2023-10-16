291790e948c5e1f08be6c334eab08257f34d74ae
Juanjo271 <juanop556@hotmail.com>
2023-10-15 21:12:01 -0500

    sesion 2-8

---
+++ Practices/Sesiones/Sesion-02-WebApi/WebDev.Api/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace WebDev.Api.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; } = null!;
+
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; } = null!;
+    }
+}
