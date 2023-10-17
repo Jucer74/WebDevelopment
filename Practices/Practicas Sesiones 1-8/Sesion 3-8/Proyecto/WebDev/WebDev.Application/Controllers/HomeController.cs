389faf61af8c94df84d5d5be3bc69b7fca5b2375
ElMecchi <matmed009@gmail.com>
2023-10-16 19:51:42 -0500

    Sesion 6

---
+++ Practices/Practicas Sesiones 1-8/Sesion 3-8/Proyecto/WebDev/WebDev.Application/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebDev.Application.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace WebDev.Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpGet]
+        [Route("[controller]/[action]")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(Login login)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    // Llamar a la API para validar el Login
+                    if (await IsValidUser(login.Email, login.Password))
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                    ModelState.AddModelError(string.Empty, "Intento de inicio de sesión no válido.");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, ex.Message);
+            }
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+        private async Task<bool> IsValidUser(string email, string password)
+        {
+            if (email.Equals("demouser@email.com") && password.Equals("Password*01"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
