3d953f61e9699d6b47d37180e83f9917f8c41545
W-sebas23 <sebasmartinezr2014@gmail.com>
2023-10-17 17:03:56 -0500

    taller sesiones

---
+++ Practices/Taller Sesiones/Sesion-01-DataBase/3-LLenar Tabla.sql
@@ -0,0 +1,24 @@
+USE UsersDB;
+
+INSERT INTO Users (Email, Name, Password, Username)
+VALUES
+('Andres.Salazar@email.com', 'Andres Salazar', 'Password', 'ASalazar'),
+('Jonathan.Asprilla@email.com', 'Jonathan Asprilla', 'Password', 'JAsprilla'),
+('Julian.Paredes@email.com', 'Julian Paredes', 'Password', 'JParedes'),
+('William.Aguirre@email.com', 'William Aguirre', 'Password', 'WAguirre'),
+('Nicolás.Alvarez@email.com', 'Nicolás Alvarez', 'Password', 'Alvarez'),
+('Maria.Carrero@email.com', 'Maria Carrero', 'Password', 'MCarrero'),
+('Nicolas.Ortiz@email.com', 'Nicolas Ortiz', 'Password', 'NOrtiz'),
+('Victor.Ospina@email.com', 'Victor Ospina', 'Password', 'VOspina'),
+('Cristian.Osorio@email.com', 'Cristian Osorio', 'Password', 'COsorio'),
+('Jeison.Garces@email.com', 'Jeison Garces', 'Password', 'JGarces'),
+('Veronica.Tofino@email.com', 'Veronica Tofino', 'Password', 'VTofino'),
+('Laura.Arango@email.com', 'Laura Arango', 'Password', 'LArango'),
+('Juan.Hernandez@email.com', 'Juan Hernandez', 'Password', 'JHernandez'),
+('Ivan.Valderrama@email.com', 'Ivan Valderrama', 'Password', 'IValderrama'),
+('Diego.Galarza@email.com', 'Diego Galarza', 'Password', 'DGalarza'),
+('Tania.Obando@email.com', 'Tania Obando', 'Password', 'TObando'),
+('Jose.Melgarejo@email.com', 'Jose Melgarejo', 'Password', 'JMelgarejo'),
+('Jhoan.Lozano@email.com', 'Jhoan Lozano', 'Password', 'JLozano'),
+('Luis.Oviedo@email.com', 'Luis Oviedo', 'Password', 'LOviedo'),
+('David.Hernandez@email.com', 'David Hernandez', 'Password', 'DHernandez');
