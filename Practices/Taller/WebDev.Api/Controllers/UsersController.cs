68d827e082c233acb27bfc5ce7ec74e10b85215b
Miguel-Marin-Moreno <103012297+Miguel-Marin-Moreno@users.noreply.github.com>
2023-10-17 15:50:09 -0500

    .
    .

--- Practices/Taller/WebDev.Api/Controllers/UsersController.cs
+++ Practices/Taller/WebDev.Api/Controllers/UsersController.cs
@@ -25,10 +25,10 @@ namespace WebDev.Api.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
-            if (_context.Users == null)
-            {
-                return NotFound();
-            }
+          if (_context.Users == null)
+          {
+              return NotFound();
+          }
             return await _context.Users.ToListAsync();
         }
 
@@ -36,10 +36,10 @@ namespace WebDev.Api.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<User>> GetUser(int id)
         {
-            if (_context.Users == null)
-            {
-                return NotFound();
-            }
+          if (_context.Users == null)
+          {
+              return NotFound();
+          }
             var user = await _context.Users.FindAsync(id);
 
             if (user == null)
@@ -86,10 +86,10 @@ namespace WebDev.Api.Controllers
         [HttpPost]
         public async Task<ActionResult<User>> PostUser(User user)
         {
-            if (_context.Users == null)
-            {
-                return Problem("Entity set 'AppDbContext.Users'  is null.");
-            }
+          if (_context.Users == null)
+          {
+              return Problem("Entity set 'AppDbContext.Users'  is null.");
+          }
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
 
