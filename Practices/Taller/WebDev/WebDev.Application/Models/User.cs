2be9ba022e3492c2234d688fbe7d1b2a2dac74c1
Miguel-Marin-Moreno <103012297+Miguel-Marin-Moreno@users.noreply.github.com>
2023-10-17 14:43:36 -0500

    .
    .

---
+++ Practices/Taller/WebDev/WebDev.Application/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebDev.Application.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El Email es obligatorio")]
+        public string Email { get; set; } = null!;
+        [Required(ErrorMessage = "El Nombre es obligatorio")]
+        public string Name { get; set; } = null!;
+        [Required(ErrorMessage = "El Nombre de usuario es obligatorio")]
+        public string Username { get; set; } = null!;
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        public string Password { get; set; } = null!;
+    }
+}
