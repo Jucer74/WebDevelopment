cec175aa8b953bcebdfc10a729e2ae7d57d56750
cris322 <94892991+cris322@users.noreply.github.com>
2023-10-16 23:55:23 -0500

    g
    g

---
+++ Proyecto/Api/WebDev.Api/WebDev.Api/Context/UsersdbContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using WebDev.Api.Model;
+
+namespace WebDev.Api.Context;
+
+public partial class UsersdbContext : DbContext
+{
+    public UsersdbContext()
+    {
+    }
+
+    public UsersdbContext(DbContextOptions<UsersdbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder);
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+     //   => optionsBuilder.UseMySql("server=localhost;database=usersdb;user=Admin;password=Admin123", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.34-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("users");
+
+            entity.Property(e => e.Email).HasMaxLength(255);
+            entity.Property(e => e.Name).HasMaxLength(50);
+            entity.Property(e => e.Password).HasMaxLength(50);
+            entity.Property(e => e.Username).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+
+    public DbSet<WebDev.Api.Model.User>? User { get; set; }
+}
