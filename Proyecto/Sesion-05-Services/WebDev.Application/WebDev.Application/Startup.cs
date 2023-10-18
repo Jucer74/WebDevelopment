88924e1619207470fdee14a435db61008010518f
httpsmarioooo <111519152+httpsmarioooo@users.noreply.github.com>
2023-10-18 18:41:31 -0500

    Sesion-05-Services inicio

---
+++ Proyecto/Sesion-05-Services/WebDev.Application/WebDev.Application/Startup.cs
@@ -0,0 +1,36 @@
+﻿namespace WebDev.Application
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "default",
+                  pattern: "{controller=Home}/{action=Index}/{id?}");
+                // Users
+                endpoints.MapControllerRoute(
+                  name: "Users",
+                  pattern: "{controller=Users}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
