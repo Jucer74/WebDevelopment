190429177283d44170d25afd027634affadbfc65
maybeimsonic <sebasshadow1@gmail.com>
2023-10-17 23:38:48 -0500

    WebDev.Api

--- Proyecto/WebDev.Api/WebDev.Api/Context/AppDbContext.cs
+++ Proyecto/WebDev.Api/WebDev.Api/Context/AppDbContext.cs
@@ -13,6 +13,15 @@
         {
         }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Reemplaza "tu_cadena_de_conexion_mysql" con tu cadena de conexión MySQL.
+                optionsBuilder.UseMySql("Server=localhost;Port=3306;Database=UsersDB;User Id=Admin;Password=Admin123;Charset=utf8;MultipleActiveResultSets=true", new MySqlServerVersion(new Version(8, 0, 23)));
+            }
+        }
+
         public DbSet<User> Users { get; set; }
     }
 }
--- Proyecto/WebDev.Api/WebDev.Api/Program.cs
+++ Proyecto/WebDev.Api/WebDev.Api/Program.cs
@@ -1,35 +1,33 @@
 using WebDev.Api.Context;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.SqlServer;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using System;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configurar la configuración
-builder.Configuration.AddJsonFile("appsettings.json", optional: false);
-
-// Obtener la cadena de conexión
-var connectionString = builder.Configuration.GetConnectionString("UsersDB");
-
-// Configurar el DbContext
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
-
-// Otros servicios y configuraciones...
+// Configurar la conexión a la base de datos
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql("Server=localhost;Port=3306;Database=UsersDB;User Id=Admin;Password=Admin123;Charset=utf8", new MySqlServerVersion(new Version(8, 0, 34))));
+builder.Services.AddSwaggerGen(s => s.SwaggerDoc("v1", new OpenApiInfo { Title = "User API", Version = "v1" }));
+// Resto del código
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-app.Run();
 
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // Enable middleware to serve generated Swagger as a JSON endpoint.
     app.UseSwagger();
-    app.UseSwaggerUI();
+
+    // Enable middleware to serve swagger-ui (HTML. JS, CSS, etc.),
+    // specifying the Swagger JSON endpoint
+    app.UseSwaggerUI(s => s.SwaggerEndpoint("/swagger/v1/swagger.json", "Users API"));
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
