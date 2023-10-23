93bc32565a0f25b17e192e1d1c1ed6180dfc5067
Harval04 <102597838+Harval04@users.noreply.github.com>
2023-10-23 18:02:11 -0500

    user and app

--- Practices/06.Taller/ReactWeb/usermanagerapp/src/App.js
+++ Practices/06.Taller/ReactWeb/usermanagerapp/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Layout } from './Layout';
 import { NoMatch } from './components/NoMatch';
 import { Home } from './components/Home';
 import { Login } from './components/Login';
-import { Users } from './components/Users';
+import { List } from './components/Users';
 import { NavigationBar } from './components/NavigationBar';
-
 
 function App() {
   return (
@@ -18,11 +16,10 @@ function App() {
         <Layout>
           <Router>
             <Routes>
-              <Route exact path="/" element={<Home/>} />
-              <Route path="/Home" element={<Home/>} />
-              <Route path="/Users" element={<Users/>} />
-              <Route path="/Login" element={<Login/>} />
-              <Route element={<NoMatch/>} />
+              <Route path="/" element={<Home />} />
+              <Route path="/Users" element={<List />} />
+              <Route path="/Login" element={<Login />} />
+              <Route element={<NoMatch />} />
             </Routes>
           </Router>
         </Layout>
