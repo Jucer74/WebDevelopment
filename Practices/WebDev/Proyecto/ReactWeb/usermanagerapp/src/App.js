8c5b80489199eef110504602bc498d146e60d850
Majo <ospinamj707@gmail.com>
2023-10-24 22:11:26 -0500

    ----

--- Practices/WebDev/Proyecto/ReactWeb/usermanagerapp/src/App.js
+++ Practices/WebDev/Proyecto/ReactWeb/usermanagerapp/src/App.js
@@ -21,8 +21,7 @@ function App() {
             <Routes>
               <Route exact path="/" element={<Home/>} />
               <Route path="/Home" element={<Home/>} />
-              <Route path="/Users" element={<Users />}/> 
-              <Route path="/Users" component={List} />
+              <Route path="/Users" component={<List/>} />
               <Route path="/Login" element={<Login/>} />
               <Route element={<NoMatch/>} />
             </Routes>
