6805ab6bdc3d4703033f0c009b168e44507e944f
W-sebas23 <sebasmartinezr2014@gmail.com>
2023-10-26 12:22:40 -0500

    nav bar

--- Proyecto/Pizzeria/webreact/src/App.js
+++ Proyecto/Pizzeria/webreact/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FaShoppingCart, FaSignInAlt } from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav class="navbar navbar-expand-sm bg-light mb-3">
+          <div class="container">
+          <a class="navbar-brand" href="#">
+            LOGO
+           </a>
+              <div class="navbar-collapse">
+                  <ul class="navbar-nav">
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">Menu</a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="#">Usuarios</a>
+                      </li>
+                  </ul>
+              </div>
+              <div class="text-right">
+                  <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                          <FaShoppingCart /> Carrito
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                          <FaSignInAlt /> Login
+                        </a>
+                    </li>
+                  </ul>
+              </div>
+          </div>
+      </nav>
     </div>
   );
 }
