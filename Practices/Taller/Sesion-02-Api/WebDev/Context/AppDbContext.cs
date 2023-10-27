9a70e398082115d7c80487a64114f78beb87919b
Neuroxcx1 <111459065+Neuroxcx1@users.noreply.github.com>
2023-10-26 20:31:07 -0500

    AVANCE TALLER

---
+++ Practices/Taller/Sesion-02-Api/WebDev/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace WebDev.Api.Context
+{
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+    using WebDev.Model;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
