75d5050305e7ba321d68fc3d905fc88967480183
Luhy Loas <luysnickers@gmail.com>
2023-10-30 10:47:27 -0500

    hh

---
+++ Practices/Sesiones/2. Api/WebDev.Api/WebDev.Api/Model/User.cs
@@ -0,0 +1,22 @@
+﻿namespace WebDev.Api.Model
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Password { get; set; } = null!;
+
+        [Required]
+        public string Username { get; set; } = null!;
+    }
+}
