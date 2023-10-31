00fdc761c0b7b725faa74104660cf145f09bca97
Michael Estiven Reyes Escobar <maik1234reyes@gmail.com>
2023-10-31 18:30:45 -0500

    Sesion 1 - 8

--- Practices/Sesiones/ReactWeb/usermanagerapp/src/App.js
+++ Practices/Sesiones/ReactWeb/usermanagerapp/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Layout } from './Components/Layout';
+import { NoMatch } from './Components/NoMatch';
+import { Home } from './Components/Home';
+import { List } from './Components/Users';
+import { Login } from './Components/Login';
+import { NavigationBar } from './Components/NavigationBar';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.Fragment>
+        <NavigationBar />
+        <Layout>
+          <Router>
+            <Routes>
+              <Route exact path="/" element={<Home/>} />
+              <Route path="/Home" element={<Home/>} />
+              <Route path="/Users" component={<List />} />
+              <Route path="/Login" element={<Login/>} />
+              <Route element={<NoMatch/>} />
+            </Routes>
+          </Router>
+        </Layout>
+      </React.Fragment>
     </div>
   );
 }
