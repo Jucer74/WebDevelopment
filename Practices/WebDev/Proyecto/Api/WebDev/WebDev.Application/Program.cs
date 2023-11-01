cb84868505e898ef397dc759bc3738fba50982f3
Majo <ospinamj707@gmail.com>
2023-10-31 21:59:11 -0500

    TALLER

--- Practices/WebDev/Proyecto/Api/WebDev/WebDev.Application/Program.cs
+++ Practices/WebDev/Proyecto/Api/WebDev/WebDev.Application/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.ervices.AddCors();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
