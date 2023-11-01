9548b4b03d8506b7387ffcf6ef64555f2d7e2682
santiago pabon <spabon1712@gmail.com>
2023-10-31 23:50:28 -0500

    correccion

--- Practices/PracticaWebdev/WebDev/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebDev.Api.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
