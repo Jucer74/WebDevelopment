d5b4f3b57badade8ac38051d8f51bb231e9bf20e
mariangel09 <angelplazamima@gmail.com>
2023-11-02 19:05:32 -0500

    Sesiones

---
+++ Practices/04.WebAPI/Sesiones/WebDev.Api/WebDev.Api/Models/UsersdbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebDev.Api.Models;
+
+public partial class UsersdbContext : DbContext
+{
+    public UsersdbContext()
+    {
+    }
+
+    public UsersdbContext(DbContextOptions<UsersdbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("users");
+
+            entity.Property(e => e.Email).HasMaxLength(255);
+            entity.Property(e => e.Name).HasMaxLength(50);
+            entity.Property(e => e.Password).HasMaxLength(50);
+            entity.Property(e => e.Username).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
