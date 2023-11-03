2953895bbd3ec9cdefb3283432b3517827bb4ebd
MiguelitoooXX <cordobamiguel2828@gmail.com>
2023-11-02 21:20:18 -0500

    done

--- Proyecto/Api/WebDev.Api/WebDev.Api/Program.cs
+++ Proyecto/Api/WebDev.Api/WebDev.Api/Program.cs
@@ -1,21 +1,31 @@
 using WebDev.Api.Context;
 using Microsoft.EntityFrameworkCore;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
-using System;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Configurar la conexión a la base de datos
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql("Server=localhost;Port=3306;Database=UsersDB;User Id=Admin;Password=Admin123;Charset=utf8", new MySqlServerVersion(new Version(8, 0, 34))));
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySQL(builder.Configuration.GetConnectionString("CnnStr")));
+
 builder.Services.AddSwaggerGen(s => s.SwaggerDoc("v1", new OpenApiInfo { Title = "User API", Version = "v1" }));
-// Resto del código
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddCors();
+
+
 var app = builder.Build();
+
+
+app.UseCors(options => {
+    options.WithOrigins("http://localhost:3000");
+    options.AllowAnyMethod();
+    options.AllowAnyHeader();
+});
+
 
 if (app.Environment.IsDevelopment())
 {
