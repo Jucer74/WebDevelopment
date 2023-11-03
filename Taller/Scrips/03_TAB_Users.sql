40552a3ba6addee265d874df79e11edaba34baac
FelipeAraujoG <felipeara04@gmail.com>
2023-11-03 09:06:15 -0500

    DB

---
+++ Taller/Scrips/03_TAB_Users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `usersdb`.`Users` (
+  `Id` INT NOT NULL AUTO_INCREMENT,
+  `Email` VARCHAR(255) NOT NULL,
+  `Name` VARCHAR(50) NOT NULL,
+  `Password` VARCHAR(50) NOT NULL,
+  `Username` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`Id`));
