8dc9e7c0b04948c2cfcb311be9176a08dcb89139
Sagudeloz <agudelosan2002@gmail.com>
2023-11-10 09:59:40 -0500

    UPDATE

--- Exams/Exam-01/Exam-01-1/MovieRankMVC/Models/User.cs
+++ Exams/Exam-01/Exam-01-1/MovieRankMVC/Models/User.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MovieRankMVC.Models;
 
