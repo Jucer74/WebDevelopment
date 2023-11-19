0c5e1e5aba9a813507772a198d79194f094833e6
httpsmarioooo <111519152+httpsmarioooo@users.noreply.github.com>
2023-11-18 22:58:38 -0500

    delete

--- Proyecto/parcial/mi-parcial/src/pages/Car.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Car = () => {
-  return (
-    <div>Car</div>
-  )
-}
-
-export default Car
--- Proyecto/parcial/mi-parcial/src/pages/Rentar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Rentar = () => {
-  return (
-    <div>Rentar</div>
-  )
-}
-
-export default Rentar
