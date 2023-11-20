5498285274660db96c8705cd237b213acd03c695
httpsmarioooo <111519152+httpsmarioooo@users.noreply.github.com>
2023-11-19 20:55:29 -0500

    Cambios Importantes

--- Proyecto/web-agencia-copia-main/src/Screens/Car.js
+++ Proyecto/web-agencia-copia-main/src/Screens/Car.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
-import styles from "./HowWeWork.module.css";
+import styles from "./Car.module.css";
 import car1 from "../images/car1.jpg";
 import car2 from "../images/car2.jpg";
 import car3 from "../images/car3.jpg";
+import car4 from "../images/car4.jpg";
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const HowWeWork = () => {
+const Car = () => {
   return (
-    <div name="HowWeWork" className={`${styles.howWeWork} text-center`}>
-      <h2 className={styles.title}>Featured Cars</h2>
+    <div name="Car" className={`${styles.car} text-center`}>
+<h2 className={styles.title} style={{ color: 'white' }}>Featured Cars</h2>
       <Row className="justify-content-center">
         {/* Card para el primer beneficio */}
         <Col xs={12} md={2}>
@@ -50,9 +52,21 @@ const HowWeWork = () => {
             </Card.Body>
           </Card>
         </Col>
+
+        <Col xs={12} md={2}>
+          <Card className={`${styles.card} mx-auto`}>
+            <Card.Img variant="top" src={car4} alt="Third slide" />
+            <Card.Body>
+              <Card.Title>Tercer Beneficio</Card.Title>
+              <Card.Text>
+                Descripción del tercer beneficio.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
       </Row>
     </div>
   );
 };
 
-export default HowWeWork;
+export default Car;
