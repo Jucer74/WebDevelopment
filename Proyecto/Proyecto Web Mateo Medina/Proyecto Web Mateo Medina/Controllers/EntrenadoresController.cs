70a5fd7ff3768dfaea3b31f77e46b9850f693b85
ElMecchi <matmed009@gmail.com>
2023-11-23 17:48:16 -0500

    vista entrenadores

---
+++ Proyecto/Proyecto Web Mateo Medina/Proyecto Web Mateo Medina/Controllers/EntrenadoresController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Proyecto_Web_Mateo_Medina.Models;
+
+namespace Proyecto_Web_Mateo_Medina.Controllers
+{
+    public class EntrenadoresController : Controller
+    {
+        private static List<Entrenador> listaEntrenadores = new List<Entrenador>
+    {
+        new Entrenador { FirstName = "Juan", LastName = "Pérez" },
+        new Entrenador { FirstName = "María", LastName = "Gómez" },
+        // Puedes agregar más entrenadores aquí...
+    };
+        // GET: EntrenadoresController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: EntrenadoresController/Details/5
+        public ActionResult Detalles(int id)
+        {
+            var entrenador = listaEntrenadores.ElementAtOrDefault(id);
+            return View(entrenador);
+        }
+
+        // GET: EntrenadoresController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EntrenadoresController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EntrenadoresController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EntrenadoresController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EntrenadoresController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EntrenadoresController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        // Acción para mostrar la lista de entrenadores disponibles
+        // Acción para mostrar la lista de entrenadores
+        public ActionResult ListaEntrenadores()
+        {
+            return View(listaEntrenadores);
+        }
+
+    }
+}
---
+++ Proyecto/Proyecto Web Mateo Medina/Proyecto Web Mateo Medina/Models/EntrenadorModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Proyecto_Web_Mateo_Medina.Models
+{
+    public class Entrenador
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
