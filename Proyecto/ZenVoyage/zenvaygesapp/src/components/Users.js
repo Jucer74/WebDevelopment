63e0b84cc258c4d1ec9e90b73fafcf799bcb55d1
Majo <ospinamj707@gmail.com>
2023-11-22 21:10:09 -0500

    User

--- Proyecto/ZenVoyage/zenvaygesapp/src/components/Users.js
+++ Proyecto/ZenVoyage/zenvaygesapp/src/components/Users.js
@@ -1,33 +1,95 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Button, Form } from 'react-bootstrap';
 
-const Users = () => {
-  const [userProfiles, setUserProfiles] = useState([]);
+const UserProfile = () => {
+  const [userProfile, setUserProfile] = useState(null);
+  const [editMode, setEditMode] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    username: '',
+  });
 
   useEffect(() => {
-    axios.get('http://localhost:3001/accounts')
+    const userId = 1;
+
+    axios.get(`http://localhost:3001/accounts/${userId}`)
       .then(response => {
-        setUserProfiles(response.data);
+        setUserProfile(response.data);
+        setFormData(response.data);
       })
       .catch(error => {
-        console.error('Error fetching user profiles:', error);
+        console.error('Error fetching user profile:', error);
       });
   }, []);
 
+  const handleEditClick = () => {
+    setEditMode(true);
+  };
+
+  const handleSaveChanges = () => {
+    setUserProfile(formData);
+    setEditMode(false);
+  };
+
+  const handleCancelEdit = () => {
+  
+    setFormData(userProfile);
+    setEditMode(false);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
     <div>
-      <h1>User Profiles</h1>
-      {userProfiles.map(user => (
-        <div key={user.id}>
-          <h3>{user.name}</h3>
-          <p>Email: {user.email}</p>
-          <p>Username: {user.username}</p>
-          {/* Agrega más detalles del perfil si están disponibles en tu API */}
-          <hr />
+      {userProfile && (
+        <div>
+          <h1>User Profile</h1>
+          <img src={userProfile.profileImage} alt="Profile" />
+          <p>Nombre: {userProfile.name}</p>
+          <p>Email: {userProfile.UserEmail}</p>
+
+          {editMode ? (
+            <Form>
+              
+              <Form.Group controlId="formName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter your name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                />
+              </Form.Group>
+
+             
+
+              <Button variant="primary" onClick={handleSaveChanges}>
+                Guardar cambios
+              </Button>
+              <Button variant="secondary" onClick={handleCancelEdit}>
+                Cancelar
+              </Button>
+            </Form>
+          ) : (
+            <div>
+              <Button variant="primary" onClick={handleEditClick}>
+                Editar perfil
+              </Button>
+            </div>
+          )}
         </div>
-      ))}
+      )}
     </div>
   );
 };
 
-export default Users;
+export default UserProfile;
